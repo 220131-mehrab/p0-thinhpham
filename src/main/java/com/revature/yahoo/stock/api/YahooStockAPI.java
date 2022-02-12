6e60f9696d8cd296e27cf4d1aedc7cde1de2b837
phamthinh14 <thinhpham0717@gmail.com>
2022-02-11 21:18:12 -0500

    Saved before turn the project to OOP

--- src/main/java/com/revature/yahoo/stock/api/YahooStockAPI.java
+++ src/main/java/com/revature/yahoo/stock/api/YahooStockAPI.java
@@ -4,8 +4,11 @@ import com.revature.yahoo.stock.api.dto.StockDto;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 public class YahooStockAPI {
@@ -20,14 +23,17 @@ public class YahooStockAPI {
         return dto;
     }
 
-    public List<HistoricalQuote> getHistory(String stockName) {
+    public List<HistoricalQuote> getHistory(String stockName, int year, String searchType) {
+        Calendar from = Calendar.getInstance();
+        Calendar to = Calendar.getInstance();
+        from.add(Calendar.YEAR, Integer.valueOf("-" + year));
         try {
             Stock stock = YahooFinance.get(stockName);
-            List<HistoricalQuote> history = stock.getHistory();
+            List<HistoricalQuote> history = stock.getHistory(from, to, getInterval(searchType));
             for (HistoricalQuote quote : history) {
-                System.out.println("=============================");
+
                 System.out.println("Symbol: " + quote.getSymbol());
-                System.out.println("Date: " + quote.getDate());
+                System.out.println("Date: " + convertDate(quote.getDate()));
                 System.out.println("High Price: " + quote.getHigh());
                 System.out.println("Low Price: " + quote.getLow());
                 System.out.println("Closed Price: " + quote.getClose());
@@ -39,10 +45,32 @@ public class YahooStockAPI {
         return null;
     }
 
+    private String convertDate(Calendar cal) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String formattedDate = format.format(cal.getTime());
+        return formattedDate;
+    }
+
+    private Interval getInterval(String searchType) {
+        Interval interval = null;
+        switch (searchType.toUpperCase()) {
+            case "MONTHLY":
+                interval = Interval.MONTHLY;
+                break;
+            case "WEEKLY":
+                interval = Interval.WEEKLY;
+                break;
+            case "DAILY":
+                interval = Interval.DAILY;
+
+        }
+        return interval;
+    }
+
     public static void main(String[] args) {
         YahooStockAPI yahooStockAPI = new YahooStockAPI();
         System.out.println(yahooStockAPI.getStock("AAPL"));
 
-        yahooStockAPI.getHistory("PLTR");
+        yahooStockAPI.getHistory("PLTR", 1, "monthly");
     }
 }
